f206a02ecbd38645d8546b95e11b63af162bc3fc
lonelynitz <vasanthmurugesan47@gmail.com>
2026-06-08 12:58:40 +0530

    fix: correct baseURL in axios instance configuration

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://task-management-server-lyart-chi.vercel.app' || '/api',
+  baseURL: 'https://task-management-server-lyart-chi.vercel.app/api' || '/api',
   headers: {
     'Content-Type': 'application/json',
   },
